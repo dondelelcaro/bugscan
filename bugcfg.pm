83fd0c76ab5968c99cf949258cd59f0de7343341
Steinar H. Gunderson <sesse@rietz>
2007-03-07 11:42:23 +0000

    Add information about version index location, and add enough information to run bugscan on merkel.

--- bugcfg.pm
+++ bugcfg.pm
@@ -14,6 +14,19 @@ if ($host eq "master" or $host eq "spohr" or $host eq "rietz") {
 	$nonUS_sources	= "/etc/debbugs/indices/nonus.sources";
 	$pseudolist	= "/org/bugs.debian.org/etc/pseudo-packages.description";
 	$nonuslist	= "/debian/home/maor/masterfiles/Packages.non-US";
+	$versionindex = "/org/bugs.debian.org/versions/indices/versions.idx";
+	$versionpkgdir = "/org/bugs.debian.org/versions/pkg";
+} elsif ($host eq "merkel") {
+	$spooldir	= "/org/bugs.debian.org/spool/db-h";
+	$maintainerlist	= "/etc/debbugs/Maintainers";
+	$debian_ftproot	= "/org/bugs.debian.org/etc/indices/ftp/testing";
+	$nonUS_ftproot	= "/org/bugs.debian.org/etc/indices/nonus/testing";
+	$debian_sources	= "/etc/debbugs/indices/ftp.sources";
+	$nonUS_sources	= "/etc/debbugs/indices/nonus.sources";
+	$pseudolist	= "/org/bugs.debian.org/etc/pseudo-packages.description";
+	$nonuslist	= "/debian/home/maor/masterfiles/Packages.non-US";
+	$versionindex = "/org/bugs.debian.org/versions/indices/versions.idx";
+	$versionpkgdir = "/org/bugs.debian.org/versions/pkg";
 } elsif ($host eq "lightning") {
 	$spooldir	= "/debian/home/iwj/debian-bugs/spool/db";
 	$maintainerlist = "/debian/debian/indices/Maintainers";
