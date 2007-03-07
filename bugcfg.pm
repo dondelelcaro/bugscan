ee367581883b881d262b317916e1511cbc794f53
Steinar H. Gunderson <sesse@rietz>
2007-03-07 17:51:51 +0000

    Start work on use strict/warnings; start with bugcfg.

--- bugcfg.pm
+++ bugcfg.pm
@@ -1,9 +1,15 @@
 #! /usr/bin/perl
 # vim: ts=4 sw=4 nowrap
 
+use strict;
+use warnings;
+package bugcfg;
+our ($spooldir,$maintainerlist,$debian_ftproot,$nonUS_ftproot,$debian_sources,$nonUS_sources,$pseudolist);
+our ($nonuslist,$versionindex,$versionpkgdir);
+
 # General configuration stuff
 
-$host=`hostname`;
+my $host=`hostname`;
 chomp $host;
 if ($host eq "master" or $host eq "spohr" or $host eq "rietz") {
 	$spooldir	= "/org/bugs.debian.org/debbugs/spool/db-h";
@@ -14,8 +20,8 @@ if ($host eq "master" or $host eq "spohr" or $host eq "rietz") {
 	$nonUS_sources	= "/etc/debbugs/indices/nonus.sources";
 	$pseudolist	= "/org/bugs.debian.org/etc/pseudo-packages.description";
 	$nonuslist	= "/debian/home/maor/masterfiles/Packages.non-US";
-	$versionindex = "/org/bugs.debian.org/versions/indices/versions.idx";
-	$versionpkgdir = "/org/bugs.debian.org/versions/pkg";
+	$versionindex   = "/org/bugs.debian.org/versions/indices/versions.idx";
+	$versionpkgdir  = "/org/bugs.debian.org/versions/pkg";
 } elsif ($host eq "merkel") {
 	$spooldir	= "/org/bugs.debian.org/spool/db-h";
 	$maintainerlist	= "/etc/debbugs/Maintainers";
@@ -27,21 +33,15 @@ if ($host eq "master" or $host eq "spohr" or $host eq "rietz") {
 	$nonuslist	= "/debian/home/maor/masterfiles/Packages.non-US";
 	$versionindex = "/org/bugs.debian.org/versions/indices/versions.idx";
 	$versionpkgdir = "/org/bugs.debian.org/versions/pkg";
-} elsif ($host eq "lightning") {
-	$spooldir	= "/debian/home/iwj/debian-bugs/spool/db";
-	$maintainerlist = "/debian/debian/indices/Maintainers";
-	$ftproot	= "/debian/debian/dists/potato/";
-	$pseudolist	= "/debian/home/maor/masterfiles/pseudo-packages.description";
-	$nonuslist	= "/debian/home/maor/masterfiles/Packages.non-US";
 } else {
 	die "Unknown machine, please configure paths in bugcfg.pm\n";
 }
 
-$btsURL			= "http://www.debian.org/Bugs/";
-@architectures		= ( "i386", "m68k", "alpha", "sparc", "powerpc", "arm", "hppa", "ia64", "mips", "mipsel", "s390" );
-@sections		= ( "main", "contrib", "non-free" );
-@priorities		= ( "serious", "grave", "critical" );
-@skiptags		= ( );
+my $btsURL			= "http://www.debian.org/Bugs/";
+my @architectures		= ( "i386", "m68k", "alpha", "sparc", "powerpc", "arm", "hppa", "ia64", "mips", "mipsel", "s390" );
+my @sections		= ( "main", "contrib", "non-free" );
+my @priorities		= ( "serious", "grave", "critical" );
+my @skiptags		= ( );
 
 1;
 
