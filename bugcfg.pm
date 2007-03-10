38875d2808a5c6ee95917d56f4c5d6b992e0be24
Steinar H. Gunderson <sesse@rietz>
2007-03-10 15:29:03 +0000

    Remove more nonUS traces.

--- bugcfg.pm
+++ bugcfg.pm
@@ -4,8 +4,8 @@
 use strict;
 use warnings;
 package bugcfg;
-our ($spooldir,$maintainerlist,$debian_ftproot,$nonUS_ftproot,$debian_sources,$nonUS_sources,$pseudolist);
-our ($nonuslist,$versionindex,$versionpkgdir);
+our ($spooldir,$maintainerlist,$debian_ftproot,$debian_sources,$pseudolist);
+our ($versionindex,$versionpkgdir);
 
 # General configuration stuff
 
@@ -15,22 +15,16 @@ if ($host eq "master" or $host eq "spohr" or $host eq "rietz") {
 	$spooldir	= "/org/bugs.debian.org/debbugs/spool/db-h";
 	$maintainerlist	= "/etc/debbugs/Maintainers";
 	$debian_ftproot	= "/org/bugs.debian.org/etc/indices/ftp/testing";
-	$nonUS_ftproot	= "/org/bugs.debian.org/etc/indices/nonus/testing";
 	$debian_sources	= "/etc/debbugs/indices/ftp.sources";
-	$nonUS_sources	= "/etc/debbugs/indices/nonus.sources";
 	$pseudolist	= "/org/bugs.debian.org/etc/pseudo-packages.description";
-	$nonuslist	= "/debian/home/maor/masterfiles/Packages.non-US";
 	$versionindex   = "/org/bugs.debian.org/versions/indices/versions.idx";
 	$versionpkgdir  = "/org/bugs.debian.org/versions/pkg";
 } elsif ($host eq "merkel") {
 	$spooldir	= "/org/bugs.debian.org/spool/db-h";
 	$maintainerlist	= "/etc/debbugs/Maintainers";
 	$debian_ftproot	= "/org/bugs.debian.org/etc/indices/ftp/testing";
-	$nonUS_ftproot	= "/org/bugs.debian.org/etc/indices/nonus/testing";
 	$debian_sources	= "/etc/debbugs/indices/ftp.sources";
-	$nonUS_sources	= "/etc/debbugs/indices/nonus.sources";
 	$pseudolist	= "/org/bugs.debian.org/etc/pseudo-packages.description";
-	$nonuslist	= "/debian/home/maor/masterfiles/Packages.non-US";
 	$versionindex = "/org/bugs.debian.org/versions/indices/versions.idx";
 	$versionpkgdir = "/org/bugs.debian.org/versions/pkg";
 } else {
