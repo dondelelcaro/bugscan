1723fbba7d45b0a0c8238bee6c035258de2e06dd
Debian BTS <debbugs@rietz>
2009-02-17 01:20:21 +0000

    switch from arm to armel

--- bugcfg.pm
+++ bugcfg.pm
@@ -26,7 +26,7 @@ if ($host eq "master" or $host eq "spohr" or $host eq "rietz") {
 	die "Unknown machine, please configure paths in bugcfg.pm\n";
 }
 
-our @architectures		= ( "i386", "amd64", "alpha", "sparc", "powerpc", "arm", "hppa", "ia64", "mips", "mipsel", "s390" );
+our @architectures		= ( "i386", "amd64", "alpha", "sparc", "powerpc", "armel", "hppa", "ia64", "mips", "mipsel", "s390" );
 our @sections		= ( "main", "contrib", "non-free" );
 our @priorities		= ( "serious", "grave", "critical" );
 our @skiptags		= ( );
