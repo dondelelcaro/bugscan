4cfafcb239f86a4a8f6d33e8fecc16c5fb9f1c04
Don Armstrong <don@donarmstrong.com>
2011-06-13 13:31:55 -0700

    cleanup configuration to use Debbugs::Config

--- bugcfg.pm
+++ bugcfg.pm
@@ -6,29 +6,23 @@ use warnings;
 package bugcfg;
 our ($spooldir,$maintainerlist,$debian_ftproot,$debian_sources,$pseudolist);
 
+use Debbugs::Config qw(:config);
+
 # General configuration stuff
 
-my $host=`hostname`;
-chomp $host;
-if ($host eq "master" or $host eq "spohr" or $host eq "rietz") {
-	$spooldir	= "/org/bugs.debian.org/debbugs/spool/db-h";
-	$maintainerlist	= "/etc/debbugs/Maintainers";
-	$debian_ftproot	= "/org/bugs.debian.org/etc/indices/ftp/testing";
-	$debian_sources	= "/etc/debbugs/indices/ftp.sources";
-	$pseudolist	= "/org/bugs.debian.org/etc/pseudo-packages.description";
-} elsif ($host eq "merkel") {
-	$spooldir	= "/org/bugs.debian.org/spool/db-h";
-	$maintainerlist	= "/etc/debbugs/Maintainers";
-	$debian_ftproot	= "/org/bugs.debian.org/etc/indices/ftp/testing";
-	$debian_sources	= "/etc/debbugs/indices/ftp.sources";
-	$pseudolist	= "/org/bugs.debian.org/etc/pseudo-packages.description";
-} else {
-	die "Unknown machine, please configure paths in bugcfg.pm\n";
-}
+$spooldir = $config{spool_dir};
+$maintainerlist = $config{maintainer_file};
+$debian_ftproot = $config{package_source}
+$debian_sources = $config{package_source};
+$pseudolist = $config{pseudo_desc_file};
+
+$debian_sources	= "/etc/debbugs/indices/ftp.sources";
+
+
 
 our @architectures		= ( "i386", "amd64", "alpha", "sparc", "powerpc", "armel", "hppa", "ia64", "mips", "mipsel", "s390" );
 our @sections		= ( "main", "contrib", "non-free" );
-our @priorities		= ( "serious", "grave", "critical" );
+our @priorities		= $config{strong_severities};
 our @skiptags		= ( );
 
 1;
